ffc6ea4c47fee3d8653957c1c5b5f9828669f28e
Aphrodis1st <aphrodisu2019@gmail.com>
2024-05-14 11:44:51 +0200

    initial commit

--- models/amenity.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""The amenity module for the BaseModel program"""
-
-from models.base_model import BaseModel
-
-class Amenity(BaseModel):
-    """A class that implements the Amenity model"""
-    name = ""
-    
--- models/state.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""The state module for the BaseModel program"""
-
-from models.base_model import BaseModel
-
-class State(BaseModel):
-    """A class that defines the attributes for the state module"""
-    
-    name = ""
